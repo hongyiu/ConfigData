181bba6e710e4d5a1f0e4d3d20eaf8a503a3032f
Yiu Yu <hy.yu@on.cc>
2024-03-27 14:12:35 +0800

    Update dependencies and enable test case

--- src/test/java/com/example/lab3server/Lab3ServerApplicationTests.java
+++ src/test/java/com/example/lab3server/Lab3ServerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.lab3server;
 
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class Lab3ServerApplicationTests {
 
-	// @Test
+	@Test
 	void contextLoads() {
+		Assert.isTrue(true, "Result should be true");
 	}
 
 }
